3d2f605f32c1fcb31cd9cb87a716c16bb68f0c16
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2020-05-07 00:55:37 +0530

    Added path to quill editor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import { Editor, EditorState, RichUtils } from 'draft-js';
+import Quill from './components/quill'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
 class App extends Component {
   constructor() {
@@ -10,6 +17,7 @@ class App extends Component {
   }
 
   onChange = (editorState) => {
+    console.log(editorState);
     this.setState({ editorState });
   }
 
@@ -24,7 +32,7 @@ class App extends Component {
   handleKeyCommand = (command) => {
     console.log(command);
     const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
-    console.log(newState);
+    // console.log(newState);
     if (newState) {
       this.onChange(newState);
       return 'handled';
@@ -34,15 +42,26 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <button onClick={this.onToggleCode}>Code Block</button>
-        <button onClick={this.onUnderlineClick}>Underline</button>
-        <Editor
-          editorState={this.state.editorState}
-          handleKeyCommand={this.handleKeyCommand}
-          onChange={this.onChange}
-        />
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <div>
+              <button onClick={this.onToggleCode}>Code Block</button>
+              <button onClick={this.onUnderlineClick}>Underline</button>
+              <Editor
+                editorState={this.state.editorState}
+                handleKeyCommand={this.handleKeyCommand}
+                onChange={this.onChange}
+              />
+            </div>
+            <br/>            
+            <Link to="/quill"><button>Quill</button></Link>
+          </Route>
+          <Route path="/quill">
+            <Quill />
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/components/quill.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Quill extends Component {
+    render() {
+       return (
+          <div>
+             <h1>Contact...</h1>
+          </div>
+       )
+    }
+ }
+ 
+ export default Quill;
